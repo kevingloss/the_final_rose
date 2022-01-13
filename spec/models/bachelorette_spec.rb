3050b992e56d2946e609cedc8e9156330ef271ff
Kevin Gloss <kgloss89@gmail.com>
2022-01-13 16:51:46 -0700

    Add hometown method

--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -13,12 +13,19 @@ RSpec.describe Bachelorette, type: :model do
     @c1 = @b1.contestants.create!(name: "Mark Johnson", age: 50, hometown: "Philadelphia, PA")
     @c2 = @b1.contestants.create!(name: "John Steel", age: 35, hometown: "Denver, CO")
     @c3 = @b1.contestants.create!(name: "Joe Jacobs", age: 40, hometown: "Miami, FL")
-    @c4 = @b1.contestants.create!(name: "Grant Gravelly", age: 55, hometown: "Houston, TX")
+    @c4 = @b1.contestants.create!(name: "GG Gravelly", age: 55, hometown: "Houston, TX")
+    @c5 = @b2.contestants.create!(name: "Gilbert Gravelly", age: 55, hometown: "Houston, TX")
+    @c6 = @b2.contestants.create!(name: "Grant Gravelly", age: 55, hometown: "Denver, CO")
+    @c7 = @b2.contestants.create!(name: "Joe Jacobs", age: 40, hometown: "Miami, FL")
   end
 
   describe 'instance methods' do
     it 'calculates the average age of contestants' do
       expect(@b1.contestant_avg_age).to eq(45)
+    end
+
+    it 'displays a unique list of hometowns for the contestants' do
+      expect(@b2.contestant_hometowns).to eq(["Houston, TX", "Denver, CO", "Miami, FL"].sort)
     end
   end
 end

5535abf781ee307e280b72049bcf31dbf7c9f250
Kevin Gloss <kgloss89@gmail.com>
2022-01-13 15:50:53 -0700

    Feat season number and description methods

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,12 @@ class Contestant < ApplicationRecord
   belongs_to :bachelorette
   has_many :contestant_outings
   has_many :outings, through: :contestant_outings
+
+  def season_num
+    bachelorette.season_number
+  end
+
+  def description
+    bachelorette.season
+  end
 end
